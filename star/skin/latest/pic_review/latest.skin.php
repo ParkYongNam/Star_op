59c22ee5d69f03e535f5844edbb2aa48ec3d28f8
ParkYongNam <dkskcka3@naver.com>
2024-02-16 11:02:39 +0900

    성형후기 최근게시의 자바스크립트 썸네일 클릭이벤트 최적화

--- star/skin/latest/pic_review/latest.skin.php
+++ star/skin/latest/pic_review/latest.skin.php
@@ -14,134 +14,476 @@ $board_file_url = G5_DATA_URL . '/file/' . $bo_table;
 
 ?>
 
+<div class ="review_img">
+    <div class="ba-wrap " style="height:435px;">
+         <div class="row">
+             <div class="large-8 columns">
+                 <div class="twentytwenty-wrapper twentytwenty-horizontal">
+                     <div class="twentytwenty-container" style="height:600px">
 
-
-    <div class="review_img " style="height:435px;">
-        <div class="reviewimg twentytwenty-container" style="position:relative overflow: hidden;" >
-    <?php
+        <?php
 
   
 
-    for ($i=0; $i<$list_count; $i++) {
+for ($i=0; $i<$list_count; $i++) {
+    
+    $img_link_html = '';
+    
+    $wr_href = get_pretty_url($bo_table, $list[$i]['wr_id']);
+
+ 
+        $thumb = get_list_thumbnail($bo_table, $list[$i]['wr_id'], $thumb_width, $thumb_height, false, true);
+
+        if($thumb['ori']) {
+            $img = $thumb['ori'];
+        } else {
+            $img = G5_IMG_URL.'/no_img.png';
+            $thumb['alt'] = '이미지가 없습니다.';
+        }
+        $img_content = '<img src="'.$img.'" alt="'.$thumb['alt'].'" >';
+        $img_link_html = '<a href="'.$wr_href.'" class="lt_img" >'.run_replace('thumb_image_tag', $img_content, $thumb).'</a>';
+
+        $list[$i]['file'] = get_file($bo_table, $list[$i]['wr_id']);
+        for($j =0; $j < count($list[$i]['file']); $j++){
+            $imgarray = thumbnail($list[$i]['file'][$j]['file'], $board_file_path, $board_file_path, $thumb_width,'', false,true);
+           if(!empty($imgarray)){
+           ?>
         
-        $img_link_html = '';
+             <img src="<?php
+           echo $board_file_url . "/" . thumbnail($list[$i]['file'][$j]['file'], $board_file_path, $board_file_path, $thumb_width,'', false,true);
+           ?>"  class='<?php echo $j === 0 ? "twentytwenty-before" : "twentytwenty-after" ?>' 
+           style="<?php echo $j === 0 ? 'clip: rect(0px, 177.156px, 364px, 0px)' : 'clip: rect(0px, 317px, 364px, 177.156px)' ?>" >
+          
+           <?php
+           }
+          
+         }
+   
+?>
         
-        $wr_href = get_pretty_url($bo_table, $list[$i]['wr_id']);
 
-     
-            $thumb = get_list_thumbnail($bo_table, $list[$i]['wr_id'], $thumb_width, $thumb_height, false, true);
 
-            if($thumb['ori']) {
-                $img = $thumb['ori'];
-            } else {
-                $img = G5_IMG_URL.'/no_img.png';
-                $thumb['alt'] = '이미지가 없습니다.';
-            }
-            $img_content = '<img src="'.$img.'" alt="'.$thumb['alt'].'" >';
-            $img_link_html = '<a href="'.$wr_href.'" class="lt_img" >'.run_replace('thumb_image_tag', $img_content, $thumb).'</a>';
+  <?php }  ?>
 
-            $list[$i]['file'] = get_file($bo_table, $list[$i]['wr_id']);
-            for($j =0; $j < count($list[$i]['file']); $j++){
-                $imgarray = thumbnail($list[$i]['file'][$j]['file'], $board_file_path, $board_file_path, $thumb_width,'', false,true);
-               if(!empty($imgarray)){
-               ?>
-            
-                 <img src="<?php
-               echo $board_file_url . "/" . thumbnail($list[$i]['file'][$j]['file'], $board_file_path, $board_file_path, $thumb_width,'', false,true);
-               ?>"  class='<?php echo $j === 0 ? "twentytwenty-before" : "twentytwenty-after" ?>' style="max-width: 100%;
-               position: absolute;
-               top: 0;
-               left:0;
-               width:%;
-               display: block;" >
-              
-               <?php
-               }
-              
-             }
-       
-    ?>
   
-  
-      <?php }  ?>
-             
-      
+
+
+
+
+                       <div class="twentytwenty-handle" ></div>
+                </div>
+            </div>
+            <div class="event-navBtn" >
+
+
+
+            <div  class="event-nav event-nav-active"> <img src="https://byulstar.com/front/images/20200828/1.jpg"></div>
+            <div  class="event-nav"> <img src="https://byulstar.com/front/images/20200828/2.jpg" /></div>
+            <div  class="event-nav"> <img src="https://byulstar.com/front/images/20200828/3.jpg"  /></div>
+            <div  class="event-nav"> <img src="https://byulstar.com/front/images/20200828/4.jpg"  /></div>
+            <div  class="event-nav"> <img src="https://byulstar.com/front/images/20200828/5.jpg"  /></div>
+            <div  class="event-nav"> <img src="https://byulstar.com/front/images/20200828/6.jpg"  /></div>
+            <div  class="event-nav"> <img src="https://byulstar.com/front/images/20200828/7.jpg"  /></div>
+            <div  class="event-nav"> <img src="https://byulstar.com/front/images/20200828/8.jpg"  /></div>
+            <div  class="event-nav"> <img src="https://byulstar.com/front/images/20200828/9.jpg"  /></div>
+            <div  class="event-nav"> <img src="https://byulstar.com/front/images/20200828/10.jpg"  /></div>
+            <div  class="event-nav"> <img src="https://byulstar.com/front/images/20200828/11.jpg"  /></div>
+            <div  class="event-nav"> <img src="https://byulstar.com/front/images/20200828/12.jpg"  /></div>
+            <div  class="event-nav"> <img src="https://byulstar.com/front/images/20200828/13.jpg"  /></div>
+            <div  class="event-nav"> <img src="https://byulstar.com/front/images/20200828/14.jpg"  /></div>
+            <div  class="event-nav"> <img src="https://byulstar.com/front/images/20200828/15.jpg"  /></div>
+            <div  class="event-nav" > <img src="https://byulstar.com/front/images/20200828/16.jpg"  /></div>
+
+
+
 
         </div>
-
-      
+        <div class="mini_0" >
+        <a href="https://byulstar.com/front/realself/list.php"><img src="https://byulstar.com/front/images/20200828/20200831_but.jpg"></a>
+        </div>
+      </div> <!--row-->
+        
     </div>
+</div>
+
+
     <script src="<?php echo G5_URL;?>/twentytwenty2/js/jquery.event.move.js"></script>
     <script src="<?php echo G5_URL;?>/twentytwenty2/js/jquery.twentytwenty.js"></script>
 
-    <script>
-        $(function () {
+    
 
 
+    <script type="text/javascript">
+$(function () {
+    var eventNav = new function () {
+        this.setIntervalTimer;
+        this.setPausedTimeout;
+        this.resumeTimer;
+        this.resumeTime = 2500;
+        this.interval = 2500;
+        this.currentIdx = 0;
+
+        this.start = function () {
+            var _self = this;
+
+            this.goRight();
+        }
+        this.direction;
+        this.goRight = function () {
+            var _self = this;
+
+            this.direction = 'right';
+
+            var $twentyContainer = $('.twentytwenty-container');
+            $('.twentytwenty-handle').stop().animate({
+                left: $twentyContainer.width(),
+            }, {
+                duration: 2000,
+                easing: 'linear',
+                step: function (now, fx) {
+                    $('.twentytwenty-before').css({
+                        clip: 'rect(0px, ' + now + 'px, ' + $twentyContainer.height() + 'px, 0px)',
+                    })
+                    $('.twentytwenty-after').css({
+                        clip: 'rect(0px, ' + $twentyContainer.width() + 'px, ' + $twentyContainer.height() + 'px, ' + now + 'px)',
+                    })
+                },
+                complete: function () {
+                    _self.onCompleteSlide();
+                },
+            });
+
+        }
+        this.onCompleteSlide = function () {
+            if (this.direction == 'right') {
+                this.goLeft();
+            } else {
+                this.goRight();
+            }
+        }
+        this.goLeft = function () {
+            var _self = this;
+            this.direction = 'left';
+
+            var $twentyContainer = $('.twentytwenty-container');
+            $('.twentytwenty-handle').stop().animate({
+                left: 0,
+            }, {
+                duration: 2000,
+                easing: 'linear',
+                step: function (now, fx) {
 
 
-    // Function to handle autoplay
-    var startAutoplay = function() {
-        var intervalId = setInterval(function() {
-            var $container = $('.reviewimg'); // Select the first .reviewimg container
-            var sliderPct = $container.find('.twentytwenty-handle').position().left / $container.width();
-            var newSliderPct = (sliderPct >= 1) ? 0 : sliderPct + 0.01; // Increment the slider position
-            $container.twentytwenty('adjustSlider', newSliderPct); // Adjust the slider position
-        }, 3000); // Change image every 3 seconds
+                    $('.twentytwenty-before').css({
+                        clip: 'rect(0px, ' + now + 'px, ' + $twentyContainer.height() + 'px, 0px)',
+                    })
+                    $('.twentytwenty-after').css({
+                        clip: 'rect(0px, ' + $twentyContainer.width() + 'px, ' + $twentyContainer.height() + 'px, ' + now + 'px)',
+                    })
+                },
+                complete: function () {
+                    _self.onCompleteSlide();
+                },
+            });
+        }
+        this.pause = function () {
+            this.stop();
 
-        // Pause autoplay when mouse enters the container
-        $('.reviewimg').on('mouseenter', function() {
-            clearInterval(intervalId);
-        });
-
-        // Resume autoplay when mouse leaves the container
-        $('.reviewimg').on('mouseleave', function() {
-            startAutoplay();
-        });
+            if (this.setPausedTimeout) {
+                clearTimeout(this.setPausedTimeout);
+            }
+            this.setPausedTimeout = setTimeout(function () {
+                eventNav.start();
+            }, this.resumeTime);
+        }
+        this.stop = function () {
+            $('.twentytwenty-handle').stop()
+        }
     };
-
-    // Initialize TwentyTwenty plugin on .reviewimg elements
-    $('.reviewimg').twentytwenty({ default_offset_pct: 0 });
-
-    // Start autoplay
-    startAutoplay();
+    eventNav.start();
 
 
+    $('.twentytwenty-handle').on('mouseover touchstart touchmove', function () {
+        eventNav.stop();
+    }).on('mouseleave touchend', function () {
+        eventNav.start();
+    });
+
+    var baImage = [
+        ["https://byulstar.com/front/images/20200828/1.jpg",
+        "https://byulstar.com/front/images/20200828/1_1.jpg"],
+        ["https://byulstar.com/front/images/20200828/2.jpg",
+        "https://byulstar.com/front/images/20200828/2_1.jpg"],
+        ["https://byulstar.com/front/images/20200828/3.jpg",
+        "https://byulstar.com/front/images/20200828/3_1.jpg"],
+        ["https://byulstar.com/front/images/20200828/4.jpg",
+        "https://byulstar.com/front/images/20200828/4_1.jpg"],
+        ["https://byulstar.com/front/images/20200828/5.jpg",
+        "https://byulstar.com/front/images/20200828/5_1.jpg"],
+        ["https://byulstar.com/front/images/20200828/6.jpg",
+        "https://byulstar.com/front/images/20200828/6_1.jpg"],
+        ["https://byulstar.com/front/images/20200828/7.jpg",
+        "https://byulstar.com/front/images/20200828/7_1.jpg"],
+        ["https://byulstar.com/front/images/20200828/8.jpg",
+        "https://byulstar.com/front/images/20200828/8_1.jpg"],
+        ["https://byulstar.com/front/images/20200828/9.jpg",
+        "https://byulstar.com/front/images/20200828/9_1.jpg"],
+        ["https://byulstar.com/front/images/20200828/10.jpg",
+        "https://byulstar.com/front/images/20200828/10_1.jpg"],
+        ["https://byulstar.com/front/images/20200828/11.jpg",
+        "https://byulstar.com/front/images/20200828/11_1.jpg"],
+        ["https://byulstar.com/front/images/20200828/12.jpg",
+        "https://byulstar.com/front/images/20200828/12_1.jpg"],
+        ["https://byulstar.com/front/images/20200828/13.jpg",
+        "https://byulstar.com/front/images/20200828/13_1.jpg"],
+        ["https://byulstar.com/front/images/20200828/14.jpg",
+        "https://byulstar.com/front/images/20200828/14_1.jpg"],
+        ["https://byulstar.com/front/images/20200828/15.jpg",
+        "https://byulstar.com/front/images/20200828/15_1.jpg"],
+        ["https://byulstar.com/front/images/20200828/16.jpg",
+        "https://byulstar.com/front/images/20200828/16_1.jpg"]
+
+    ];
 
 
-// $(".reviewimg").twentytwenty();
+   
+ 
+    
 
-// // Set autoplay interval
-// var autoplayInterval = setInterval(function() {
-//     $(".reviewimg").twentytwenty("move", 1); // Move to the next image
-// }, 3000); // Change image every 3 seconds
+    // var imgTarget = $(".twentytwenty-container").find("img:first").attr("src");
 
-// // Pause autoplay when mouse enters the comparison container
-// $(".reviewimg").on("mouseenter", function() {
-//     clearInterval(autoplayInterval);
-// });
+    
 
-// // Resume autoplay when mouse leaves the comparison container
-// $(".reviewimg").on("mouseleave", function() {
-//     autoplayInterval = setInterval(function() {
-//         $(".reviewimg").twentytwenty("move", 1); // Move to the next image
-//     }, 3000); // Change image every 3 seconds
-// });
-
-
-// // var moviestick = 0;
-// // setInterval(()=>{
-// //     moviestick += 0.01;  // 0~1
-// //     $('.reviewimg').twentytwenty({ default_offset_pct : moviestick });
-// // }, 100)
-
-// console.log($('.reviewimg').twentytwenty)
+    $(".event-nav").on("click", function () {
+        $(".event-nav").removeClass("event-nav-active")
+        $(this).addClass("event-nav-active")
+       
+        
+        $(".twentytwenty-container").find(".twentytwenty-before").attr("src", baImage[$(this).index()][0]);
+        $(".twentytwenty-container").find(".twentytwenty-after").attr("src", baImage[$(this).index()][1]);
+        
+    });
 
 
-          
-        })
-    </script>
+  
+
+    
 
 
+   
+    
+      
+    
+
+   
+
+
+
+
+})
+</script>
+
+<style>
+
+
+
+.ba-container {
+    width:100%;
+    height: 600px;
+    background: #f1f1f1;
+    padding: 2px 0 6px 0;
+}
+.evnetPage-wrap {
+    max-width:1064px;
+    margin: 0 auto;
+    position: relative;
+	margin-left: 26px;
+	top: -39px;
+
+}
+    .ba-wrap {
+    width:376px;
+    height:435px;
+    /*background: #fff;*/
+    margin: 0px auto;
+    position: relative;
+	margin-top: 126px;
+}
+
+
+
+.event-navBtn{
+    text-align: center;
+}
+.event-nav {
+    
+    transition: .3s;
+    cursor: pointer;
+}
+
+.event-nav img{
+    width: 97px;
+    height: 113px;
+    
+    /*border-radius: 100px;*/
+    float: left;
+    /*margin: 25px 3px;*/
+	margin: 2px 2px 0px 0px;
+    transition: .3s;
+    cursor: pointer;
+}
+
+.event-nav-active {
+	background-color: #a89a6d;
+        opacity: 0.3;
+    /*width:27px;*/
+   
+    cursor: default;
+	
+}
+
+
+
+
+
+
+/***********BEFORE AFTER ***************/
+
+
+
+
+
+
+
+
+
+
+.event-container {
+    padding: 100px 0;
+    background: #f2f2f4;
+}
+
+.event-title {
+    text-align: left;
+    font-size: 45px;
+    line-height: 55px;
+    color: #333;
+    padding-bottom: 50px;
+    display: inline-block;
+}
+
+.event-rec-top {
+    text-align: left;
+    position: relative;
+    max-width: 1200px;
+    margin: 0 auto;
+}
+.arrow-leftbtn {
+    position: absolute;
+    right: 90px;
+    bottom: 50px;
+}
+.arrow-rightbtn {
+    position: absolute;
+    right: 30px;
+    bottom: 50px;
+}
+.arrow-btn img:last-child {
+    margin-left: 20px;
+}
+
+
+.event-carousel {
+    position: relative;
+    width:100%;
+    height: 400px;
+    overflow: hidden;
+}
+
+.event-carousel ul {
+    width:100%;
+    height:100%;
+    overflow: hidden;
+    margin: 0;
+    padding: 0;
+}
+.event-carousel ul li {
+    width:280px; 
+    height:400px;
+    display:inline-block;
+    text-align: center;
+    position: absolute;
+    cursor: pointer;
+    transition: .3s all;
+}
+.event-carousel ul li:hover {
+    background: #333;
+    transition: .3s all;
+}
+.event-carousel ul li:hover .carousel-title{
+    color:#fff;
+}
+
+
+.carousel-title{
+    font-size: 20px;
+    color: #333;
+    line-height: 25px;
+    padding: 55px 0 30px 20px;
+    text-align: left;
+}
+.event-carousel ul li:nth-child(2){
+    left: 300;
+}
+.event-carousel ul li:nth-child(3){
+    left: 600;
+}
+.event-carousel ul li:nth-child(4){
+    left: 900;
+}
+.event-carousel ul li:nth-child(5){
+    left: 1200;
+}
+
+
+
+
+
+
+
+
+.event-rec {
+    width:280px;
+    height: 400px;
+    background: #fff;
+    display: inline-block;
+    margin-right: 20px;
+    cursor: pointer;
+    transition: .3s ease;
+    text-align: center;
+    
+}
+.event-rec img {
+    transition: 1s ease;
+}
+.event-rec:hover {
+    background: #333;
+    color: #fff;
+    transition: .3s ease;
+}
+
+
+
+
+
+.event-rec-title {
+    font-family: SourceHanSansK;
+    font-size: 20px;
+    font-weight: 300;
+    text-align: left;
+    padding: 50px 0 29px 19px;
+    line-height: 30px;
+}
+</style>
 
 
