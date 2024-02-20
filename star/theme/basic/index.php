214d4253b9a2027101f56a1eb4c9168b67fa2814
ParkYongNam <dkskcka3@naver.com>
2024-02-20 17:40:32 +0900

    개인정보 및 상담 form

--- star/theme/basic/index.php
+++ star/theme/basic/index.php
@@ -56,5 +56,80 @@ include_once(G5_THEME_PATH.'/head.php');
     </div>
 
 </div>
+
+<div id="section_qc">
+<div class="quick_consult_top"></div>
+  <div class="quick_consult">
+      <form id="qcf" action="#" method="post">
+        <div class="inner d-flex ">
+          <ul>
+            <li class="qce_qc2 d-flex align-items-center">
+              <label>
+                <input type="text" name="qc_name" id="qc_name" placeholder="이름">
+              </label>
+            </li>
+            <li class="qce_qc3 d-flex align-items-center mx-5">
+                <label style="padding-left:0.5rem;">
+                  <input type="text" name="qc_tel" id="qc_tel" _onkeypress="return numkeyCheck(event)">
+                </label>
+            </li>
+            <li class="qce_qc1">
+              <label>
+                <input type="radio" name="qc_type" vaule="phone" checked></input>
+                "전화상담"
+              </label>
+              <label>
+                <input type="radio" name="qc_type" vaule="kakao"></input>
+                "카톡상담"
+              </label>
+              <p class="qc_agree">
+                <label>
+                  <input type="checkbox" vaule="check" name="check" checked id="qc_agree"></input>
+                  "개인정보취급방침에 동의합니다."
+                </label>
+              </p>
+            </li>
+          </ul>
+          <a href="#" class="btn mx-3">
+            <span style="color:#fff;">상담요청</span>
+          </a>
+        </div>
+      </form>
+  </div>
+
+</div>
+<div class="main_end"></div>
+<script>
+document.addEventListener("DOMContentLoaded", function() {
+    var quick_consult_effect = 0;
+    var scrollEle_qc = $('.quick_consult_top').offset().top;
+    var width_size = $(window).outerWidth();
+
+    var qc_scrollset = function() {
+        var scrollPresent = $(document).scrollTop();
+        var windowHeight = $(window).height();
+        var scrollEff = (scrollEle_qc - windowHeight) + 100;
+        if (scrollPresent >= scrollEff && quick_consult_effect == 1) {
+            $('.quick_consult').removeClass('quick_fixed');
+            quick_consult_effect = 0;
+        } else if (scrollPresent < scrollEff && quick_consult_effect == 0) {
+            $('.quick_consult').addClass('quick_fixed');
+            quick_consult_effect = 1;
+        }
+    };
+
+    $(window).on('resize', function() {
+        scrollEle_qc = $('.quick_consult_top').offset().top;
+    });
+
+    $(document).scroll(function() {
+        qc_scrollset();
+    });
+
+    qc_scrollset();
+
+    
+});
+</script>
 <?php
 include_once(G5_THEME_PATH.'/tail.php');
